78a99c5a02f2a0ff76415e420353e625a75af2cc
pitodb <pitodb@gmail.com>
2024-05-10 15:55:24 -0400

    cambio in note engine

--- note_engine.py
+++ note_engine.py
@@ -17,5 +17,5 @@ def save_note(note):
 note_engine = FunctionTool.from_defaults(
     fn=save_note,
     name="note_saver",
-    description="this tool can save a text based note to a file for the user",
+    description="pietro del bianco",
 )
